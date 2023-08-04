d24711a94212880d4ad6102ed22518df83cfd03c
lmmv9411 <luismmv9411@gmail.com>
2023-08-04 10:12:52 -0500

    ajustes

--- assets/main/componentes/lista.jsx
+++ assets/main/componentes/lista.jsx
@@ -5,16 +5,37 @@ export class Lista extends Componente {
         super({ ...props, items: [] })
     }
 
+    montado() {
+        this.items = [
+            "list-group-item list-group-item-primary",
+            "list-group-item list-group-item-secondary",
+            "list-group-item list-group-item-success",
+            "list-group-item list-group-item-danger",
+            "list-group-item list-group-item-warning",
+            "list-group-item list-group-item-info",
+            "list-group-item list-group-item-light",
+            "list-group-item list-group-item-dark"
+        ]
+    }
+
     agregarItem(item) {
+
+        const i = Math.floor((Math.random() * this.items?.length));
+
+        const color = this.items[i];
+
+        item = { v: item, c: color }
+
         const { items } = this.state;
+
         this.update({ items: [item, ...items] })
     }
 
     render(props) {
 
         return (
-            <ul>
-                {props.items?.map(item => <li key={item}>{item}</li>)}
+            <ul className="list-group list-group-flush">
+                {props.items?.map(item => <li key={item.v} className={item.c}>{item.v}</li>)}
             </ul>
         )
     }
