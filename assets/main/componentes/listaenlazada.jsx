e3707e1188122d7f80353a3d9ab6cd9be3e43217
lmmv9411 <luismmv9411@gmail.com>
2023-08-09 18:43:16 -0500

    ajustes linked list

--- assets/main/componentes/listaenlazada.jsx
+++ assets/main/componentes/listaenlazada.jsx
@@ -10,93 +10,131 @@ export class ListaEnlazada extends Componente {
     render(props) {
         return (
             <>
-                <header>
+                <header className="p-3">
                     <h1>Cabeza: {props.cabeza?.valor}</h1>
                     <h1>Cola: {props.cola?.valor}</h1>
                     <h2>Tamaño: {this.size ?? 0}</h2>
-                    <section>
-                        <input type="text" id="txtValor" $ref="txtValor"
-                            placeholder="Valor" value={props.valor}
-                            onchange={(e) => { this.update({ valor: e.target.value }) }} />
-                        <button type="button" onclick={() => {
+                    <section className="d-flex gap-3 align-items-end flex-wrap">
 
-                            this.size++;
+                        <input
+                            className="form-control text-light bg-dark"
+                            type="text"
+                            id="txtValor"
+                            $ref="txtValor"
+                            placeholder="Valor"
+                            value={props.valor}
+                            onchange={e => this.update({ valor: e.target.value })}
+                        />
 
-                            let { cabeza, cola, valor } = this.state;
+                        <button
+                            className="btn btn-primary"
+                            type="button"
+                            onclick={() => {
 
-                            if (cola === null) {
-                                cola = nodo(valor)
-                                cabeza = cola;
-                                this.update({ cabeza, cola, valor: "" })
-                            } else {
-                                const tmp = nodo(valor)
-                                tmp.anterior = cola
-                                cola.siguiente = tmp;
-                                this.update({ cola: tmp, valor: "" })
-                            }
+                                this.size++;
 
-                            this.txtValor.focus()
+                                let { cabeza, cola, valor } = this.state;
 
-                        }}>Agregar Cola</button>
+                                if (cola === null) {
+                                    cola = nodo(valor)
+                                    cabeza = cola;
+                                    this.update({ cabeza, cola, valor: "" })
+                                } else {
+                                    const tmp = nodo(valor)
+                                    tmp.anterior = cola
+                                    cola.siguiente = tmp;
+                                    this.update({ cola: tmp, valor: "" })
+                                }
 
-                        <button type="button" onclick={() => {
+                                this.txtValor.focus()
 
-                            this.size++;
+                            }}>Agregar Cola</button>
 
-                            let { cabeza, valor, cola } = this.state;
+                        <button
+                            className="btn btn-primary"
+                            type="button"
+                            onclick={() => {
 
-                            if (cabeza === null) {
-                                cabeza = nodo(valor)
-                                cola = cabeza
-                                this.update({ cabeza, cola, valor: "" })
-                            } else {
-                                let tmp = nodo(valor);
-                                tmp.siguiente = cabeza;
-                                cabeza.anterior = tmp;
-                                this.update({ cabeza: tmp, valor: "" })
-                            }
-                            this.txtValor.focus()
+                                this.size++;
 
-                        }}>Agregar Cabeza</button>
+                                let { cabeza, valor, cola } = this.state;
+
+                                if (cabeza === null) {
+                                    cabeza = nodo(valor)
+                                    cola = cabeza
+                                    this.update({ cabeza, cola, valor: "" })
+                                } else {
+                                    let tmp = nodo(valor);
+                                    tmp.siguiente = cabeza;
+                                    cabeza.anterior = tmp;
+                                    this.update({ cabeza: tmp, valor: "" })
+                                }
+                                this.txtValor.focus()
+
+                            }}>Agregar Cabeza</button>
 
 
                         <input
+                            className="form-control text-light bg-dark"
                             type="number"
                             placeholder="posición"
                             value={props.pos}
-                            min={0}
+                            min={1}
                             max={this.size ?? 0}
-                            onchange={(e) => this.update({ pos: e.target.value })}
+                            onchange={(e) => this.update({ pos: Number(e.target.value) })}
                         />
 
-                        <button type="button" onclick={() => {
-                            
-                            let tmp = this.state.cabeza;
-                            let idx = 0;
-                            const pos = Number(this.state.pos);
+                        <button
+                            className="btn btn-primary"
+                            type="button"
+                            onclick={() => {
 
-                            while (tmp) {
-                                if (idx === pos) {
-                                    const tmpNodo = nodo(this.state.valor);
-                                    tmpNodo.siguiente = tmp.siguiente
-                                    tmpNodo.anterior = tmp;
-                                    tmp.siguiente = tmpNodo;
-                                    this.update({})
-                                    break;
-                                } else {
-                                    tmp = tmp.siguiente;
+                                let tmp = this.state.cabeza;
+                                let idx = 0;
+                                const pos = this.state.pos === 0 ? 0 : this.state.pos - 1;
+
+                                while (tmp) {
+                                    if (idx === pos) {
+                                        const tmpNodo = nodo(this.state.valor);
+                                        //cola
+                                        if (tmp.siguiente === undefined && tmp.anterior !== undefined) {
+                                            const c = this.state.cola;
+                                            c.siguiente = tmpNodo
+                                            tmpNodo.anterior = c;
+                                            this.size++;
+                                            this.update({ cola: tmpNodo })
+                                        } else if (tmp.anterior === undefined && tmp.siguiente !== undefined) {
+                                            //cabeza
+                                            const c = this.state.cabeza;
+                                            c.anterior = tmpNodo
+                                            tmpNodo.siguiente = c;
+                                            this.size++;
+                                            this.update({ cabeza: tmpNodo })
+                                        } else {
+                                            tmpNodo.siguiente = tmp;
+                                            tmpNodo.anterior = tmp.anterior;
+                                            tmp.anterior.siguiente = tmpNodo
+                                            tmp.anterior = tmpNodo;
+                                            this.size++;
+                                            this.update({})
+                                        }
+                                        this.update({ valor: "" })
+                                        this.txtValor.focus()
+                                        break;
+                                    } else {
+                                        tmp = tmp.siguiente;
+                                    }
+                                    idx++;
                                 }
-                                idx++;
-                            }
 
-                        }}>Agregar en</button>
+                            }}>Agregar en</button>
 
                     </section>
                 </header>
-                <article>
-                    <ul>
+                <article className="p-3">
+                    <ol className="list-group">
                         {leerCabeza(props.cabeza)}
-                    </ul>
+                    </ol>
                 </article>
             </>
         )
@@ -108,7 +146,7 @@ function leerCabeza(nodo) {
     let tmpNodo = nodo;
 
     while (tmpNodo) {
-        tmp.push(<li key={tmpNodo.valor}>{tmpNodo.valor}</li>)
+        tmp.push(<li className="list-group-item bg-dark text-light" key={tmpNodo.valor}>{tmpNodo.valor}</li>)
         tmpNodo = tmpNodo.siguiente
     }
 
