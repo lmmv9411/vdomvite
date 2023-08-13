7972b3fadb1c95e51bdf53470d118d04610e6d50
lmmv9411 <luismmv9411@gmail.com>
2023-08-13 08:12:15 -0500

    ajustes diseño

--- assets/main/componentes/almacen.jsx
+++ assets/main/componentes/almacen.jsx
@@ -1,6 +1,7 @@
 import { Componente } from "../../vdom/Componente";
 import { Link, Links, Router, navigateTo } from "../../vdom/Router";
 import { Fragment } from "../../vdom/VDom";
+import menu from "../estilos/menu.module.css"
 
 export class Almacen extends Componente {
     constructor(props) {
@@ -16,13 +17,12 @@ export class Almacen extends Componente {
         return (
             <Router idContenedor="test" pathBase="Almacen">
 
-                <Links>
-                    <Link to="" name="home" titulo="Almacén">Home</Link>
-                    <Link to="tetas" name="tetas" titulo="Tetas">Tetas</Link>
+                <Links className="d-flex flex-wrap" style={{ listStyle: "none" }}>
+                    <Link className={menu.link} to="" name="home" titulo="Almacén">Home</Link>
+                    <Link className={menu.link} to="tetas" name="tetas" titulo="Tetas">Tetas</Link>
                 </Links>
 
-                <div id="test">
-                    <span>Hola perros</span>
+                <div id="test" className="p-3">
                 </div>
             </Router>
         )
