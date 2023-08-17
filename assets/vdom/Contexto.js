b8e566c7deb66893680a2edac8210949e01e0939
lmmv9411 <luismmv9411@gmail.com>
2023-08-16 23:01:46 -0500

    ajuste contexto

--- assets/vdom/Contexto.js
+++ assets/vdom/Contexto.js
@@ -9,6 +9,9 @@ export class CrearContexto {
          * @param {Array.<{type:string, children:Array.<Object>, props:Object}>} children
          */
         this.Provider = (props, children) => {
+            if (Object.keys(this.children).length > 0) {
+                return;
+            }
             children.forEach(ch => {
                 const name = ch.constructor.name;
                 if (name !== "Object") {
