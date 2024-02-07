5c655e222d34ac90d158e4d74431632d1487bb87
lmmv <lmmv9411@gmail.com>
2024-02-06 21:16:11 -0500

    opt code

--- assets/main/main.jsx
+++ assets/main/main.jsx
@@ -1,6 +1,4 @@
 import { insertarElemento } from "../vdom/VDom"
 import { App } from "./componentes/app";
 
-const app = document.getElementById("app")
-
-insertarElemento(app, <App />)
+insertarElemento(document.getElementById("app"), <App />)
